1da5c01560aa2c036649b2dd30cbb3245898d44f
MOUAD EL HADDAD <mouadel28@gmail.com>
2022-02-09 14:41:32 +0100

    mod sett

--- Crypto_site/settings.py
+++ Crypto_site/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_otp.middleware.OTPMiddleware',
 ]
 
 ROOT_URLCONF = 'Crypto_site.urls'
