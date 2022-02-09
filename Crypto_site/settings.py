58018fbbe622a9ffee1a4b189a17932ff2d1795a
MOUAD EL HADDAD <mouadel28@gmail.com>
2022-02-10 00:11:02 +0100

    heroku

--- Crypto_site/settings.py
+++ Crypto_site/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 '''STATICFILES_DIRS = [
     BASE_DIR / "static",
-]'''
+]
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')'''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
